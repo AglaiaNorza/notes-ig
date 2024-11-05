b65ff6e62907bb9b44a270a564f4238f582ba3dc
AglaiaNorza <thisisaglaia@gmail.com>
2024-11-05 18:22:26 +0100

    sync: 05-11

--- notesync.sh
+++ notesync.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # script to push my handwritten notes to my gh repo 
 
-# to be safe
-rclone sync gdrive:GoodNotes/ ~/Documents/gdrive
-
 source="$HOME/Documents/gdrive"
 dest="$HOME/Documents/uni/notes-ig"
 
@@ -12,7 +9,7 @@ declare -A notes
 notes["$source/algebra/algebra ALGEBRA.pdf"]="secondo anno/algebra.pdf"
 notes["$source/algebra/REFILE/algebra FORMULE.pdf"]="secondo anno/algebra formule.pdf"
 notes["$source/algebra/REFILE/algebra ES SHEET.pdf"]="secondo anno/algebra es sheet.pdf"
-#notes["$source/algebra/REFILE/'algebra DIMOSTRAZIONI'.pdf'"]="'secondo anno'/'algebra dimostrazioni'.pdf"
+#notes["$source/algebra/REFILE/algebra DIMOSTRAZIONI.pdf"]=secondo anno/algebra dimostrazioni.pdf"
 notes["$source/probabilità/probabilità appunti.pdf"]="secondo anno/calcolo delle probabilità.pdf"
 
 for file in "${!notes[@]}"; do
