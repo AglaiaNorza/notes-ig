5cc88a18d807d22477122670a16e25dafe999040
aglaia <thisisaglaia@gmail.com>
2025-09-27 10:34:05 +0200

    sync: 27-09

--- notesync.sh
+++ notesync.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # script to push my handwritten notes to my gh repo
 
-source="$HOME/Documents/gdrive"
+drive="$HOME/Documents/gdrive"
+latex="$HOME/Documents/uni/latex"
 dest="$HOME/Documents/uni/notes-ig"
 
-if ! mount | grep "gdrive" > /dev/null; then
-    rclone mount --daemon gdrive:GoodNotes/ ~/Documents/gdrive
-fi
+#if ! mount | grep "gdrive" > /dev/null; then
+#    rclone mount --daemon gdrive:GoodNotes/ ~/Documents/gdrive
+#fi
 
 # did not need to declare a fancy array for this but i've never used bash arrays before so i wanted to try !
 declare -A notes
-notes["$source/algebra/algebra ALGEBRA.pdf"]="secondo anno/algebra.pdf"
-notes["$source/algebra/REFILE/algebra FORMULE.pdf"]="secondo anno/algebra formule.pdf"
-notes["$source/algebra/REFILE/algebra ES SHEET.pdf"]="secondo anno/algebra es sheet.pdf"
-#notes["$source/algebra/REFILE/algebra DIMOSTRAZIONI.pdf"]=secondo anno/algebra dimostrazioni.pdf"
-notes["$source/probabilità/probabilità appunti.pdf"]="secondo anno/calcolo delle probabilità.pdf"
+#notes["$drive/algebra/algebra ALGEBRA.pdf"]="secondo anno/algebra.pdf"
+#notes["$drive/algebra/REFILE/algebra FORMULE.pdf"]="secondo anno/algebra formule.pdf"
+#notes["$drive/algebra/REFILE/algebra ES SHEET.pdf"]="secondo anno/algebra es sheet.pdf"
+#notes["$drive/algebra/REFILE/algebra DIMOSTRAZIONI.pdf"]=secondo anno/algebra dimostrazioni.pdf"
+#notes["$drive/probabilità/probabilità appunti.pdf"]="secondo anno/calcolo delle probabilità.pdf"
+
+notes["$latex/automi/automi.pdf"]="terzo anno/automi.pdf"
+notes["$latex/logmat/logmat.pdf"]="terzo anno/logica matematica.pdf"
+notes["$latex/ldp/ldp.pdf"]="terzo anno/linguaggi di programmmazione.pdf"
 
 for file in "${!notes[@]}"; do
     cp "$file" "$dest/${notes[$file]}" || { echo "$file failed"; exit 1; }
@@ -27,4 +32,3 @@ if [ $? -eq 0 ] && [[ $(git status --porcelain) ]]; then
 else
     echo "no changes in the notes!"
 fi
-    
