0ae11565dd415cb925a4f15df9f1e16e8b3fd692
AglaiaNorza <thisisaglaia@gmail.com>
2025-10-06 21:02:36 +0200

    sync: 06-10

--- notesync.sh
+++ notesync.sh
@@ -5,9 +5,9 @@ drive="$HOME/Documents/gdrive"
 latex="$HOME/Documents/uni/latex"
 dest="$HOME/Documents/uni/notes-ig"
 
-#if ! mount | grep "gdrive" > /dev/null; then
-#    rclone mount --daemon gdrive:GoodNotes/ ~/Documents/gdrive
-#fi
+if ! mount | grep "gdrive" > /dev/null; then
+    rclone mount --daemon gdrive:GoodNotes/ ~/Documents/gdrive
+fi
 
 # did not need to declare a fancy array for this but i've never used bash arrays before so i wanted to try !
 declare -A notes
@@ -16,6 +16,8 @@ declare -A notes
 #notes["$drive/algebra/REFILE/algebra ES SHEET.pdf"]="secondo anno/algebra es sheet.pdf"
 #notes["$drive/algebra/REFILE/algebra DIMOSTRAZIONI.pdf"]=secondo anno/algebra dimostrazioni.pdf"
 #notes["$drive/probabilità/probabilità appunti.pdf"]="secondo anno/calcolo delle probabilità.pdf"
+
+#notes["$drive/algebra/algebra ALGEBRA.pdf"]="secondo anno/algebra.pdf"
 
 notes["$latex/automi/automi.pdf"]="terzo anno/automi.pdf"
 notes["$latex/logmat/logmat.pdf"]="terzo anno/logica matematica.pdf"
